3bab7fa81c37a30cac5fc9c754a94b426b999299
Javier Lopez <jvrlpzd@gmail.com>
2022-11-20 22:59:27 +0100

    Defined some components, created router and nav-bar beginning

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { IoMdClose } from "react-icons/io";
+import { CgMenuRight } from "react-icons/cg";
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  return <nav>
+    {/* nav open button */}
+    <div className='text-3xl cursor-pointer'>
+      <CgMenuRight />
+    </div>
+    {/* menu */}
+    <div className="bg-white shadow-2xl w-full absolute top-0 right-0 max-w-xs h-screen z-20">
+      {/* icon */}
+      <div>icon</div>
+      {/* menu list */}
+      <ul className='h-full flex flex-col justify-centeritems-center gap-y-8 text-primary font-primary font-bold text-3xl'>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+        <li>
+          <Link to='/portfolio'>Portfolio</Link>
+        </li>
+        <li>
+          <Link to='/contact'>Contact</Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
 };
 
 export default MobileNav;
